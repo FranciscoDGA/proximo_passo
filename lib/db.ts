1ae26730e543aaa7d5c34d0c32f7cc48e955c49a
Francisco Gomes <afiliadopebas@gmail.com>
2026-07-19 01:12:36 -0300

    fix: instanciar PrismaClient via Proxy lazy load

--- lib/db.ts
+++ lib/db.ts
@@ -4,10 +4,16 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+// @ts-ignore
+export const db = new Proxy({} as PrismaClient, {
+  get(target, prop) {
+    if (!globalForPrisma.prisma) {
+      globalForPrisma.prisma = new PrismaClient({
+        log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      });
+    }
+    return (globalForPrisma.prisma as any)[prop];
+  }
+}) as PrismaClient;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
